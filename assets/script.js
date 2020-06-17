f66c151d7fe52b09f8fb6ee8e075f5aef5b3959a
Jordan Long <jblong27@gmail.com>
2020-06-17 18:05:42 -0500

    pushing function to display biography and powerstats text in the respective divs

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,33 @@
-//requires jQuery
-var data = {
-    'url': 'https://audd.tech/example1.mp3',
-    'return': 'apple_music,spotify',
-    'api_token': 'test'}
+// function displayHeroInfo() {   
+    var hero = "deadpool"; 
+    var queryURLOne = "https://www.superheroapi.com/api.php/10158163759470734/search/" + hero;
+    var queryURLTwo = "https://api.giphy.com/v1/gifs/search?api_key=UZ1q06vU6ySOGMpaTwRtjIXmWHoGeJjg&q=" + hero + "&limit=25&offset=0&rating=G&lang=en";
+    //superhero API call
+    $.ajax({
+        url: queryURLOne,
+        method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            // this section will populate the powers-div with the hero's powerstats
+            var powersDiv = $(".powers-div");
+            var powerStats = JSON.stringify(response.results[0].powerstats);
+            var powerPtag = $("<p>").text(powerStats);
+            powersDiv.append(powerPtag);
+            // this section will populate the bio-div with the hero's biography
+            var bioDiv = $("#bio-div");
+            var biography = JSON.stringify(response.results[0].biography);
+            var bioPtag = $("<p>").text(biography);
+            bioDiv.append(bioPtag);
 
-var hero = "superman"
-var queryURLOne = "https://www.superheroapi.com/api.php/10158163759470734/search/" + hero;
-var queryURLTwo = "https://api.giphy.com/v1/gifs/search?api_key=UZ1q06vU6ySOGMpaTwRtjIXmWHoGeJjg&q=" + hero + "&limit=25&offset=0&rating=G&lang=en";
+        });
+    //giphy API call
+    $.ajax({
+        url: queryURLTwo,
+        method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
 
-$.getJSON('https://api.audd.io/?jsonp=?', data, function(result){
-    console.log(result);
-});
-​
-    
-$.ajax({
-    url: queryURLOne,
-    method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        console.log("test1");
-      });
-​
-​
-$.ajax({
-    url: queryURLTwo,
-    method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        console.log("test2");
-      });
+// }
+
+// $("#submit-btn").on("click", displayHeroInfo);
