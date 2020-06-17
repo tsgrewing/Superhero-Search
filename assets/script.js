c6d835faf57bcb97f5985784984f3c27c99f4e6e
Jordan Long <jblong27@gmail.com>
2020-06-17 18:09:57 -0500

    pushing function to display biography and powerstats text in the respective divs

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // function displayHeroInfo() {   
     var hero = "deadpool"; 
     var queryURLOne = "https://www.superheroapi.com/api.php/10158163759470734/search/" + hero;
-    var queryURLTwo = "https://api.giphy.com/v1/gifs/search?api_key=UZ1q06vU6ySOGMpaTwRtjIXmWHoGeJjg&q=" + hero + "&limit=25&offset=0&rating=G&lang=en";
+    var queryURLTwo = "https://api.giphy.com/v1/gifs/search?api_key=UZ1q06vU6ySOGMpaTwRtjIXmWHoGeJjg&q=" + hero + "&limit=5&offset=0&rating=G&lang=en";
     //superhero API call
     $.ajax({
         url: queryURLOne,
