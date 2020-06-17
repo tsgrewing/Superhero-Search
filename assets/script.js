f730d1fe3814b19c6a56ed10f9d80e157635a0c5
Alix Wawa <alixwawa@yahoo.com>
2020-06-16 20:11:11 -0500

    adding jquery link

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//requires jQuery
+var data = {
+    'url': 'https://audd.tech/example1.mp3',
+    'return': 'apple_music,spotify',
+    'api_token': 'test'}
+
+$.getJSON('https://api.audd.io/?jsonp=?', data, function(result){
+    console.log(result);
+});
