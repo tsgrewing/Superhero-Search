61aaf1ab9d80d6d1298348e706c359d5ac28635c
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-06-20 11:24:11 -0400

    updating media queries for small screens

--- assets/script.js
+++ assets/script.js
@@ -104,7 +104,6 @@ function displayHeroInfo() {
                             color: 'black'
                         }
                     }
-                    
                 }
             });
 
