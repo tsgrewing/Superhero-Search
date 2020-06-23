9f1065d8d16937f24186263f46cf09b2bfd9a42b
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-06-22 23:59:15 -0400

    setting up array for loading character on load

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,22 @@ $(document).ready(function() {
 // this function provides a dropdown autocomplete menu on the search bar
 function searchAutoComplete() {
     jQuery.get('assets/heros.txt', function(data) {
-        heroArray = data.toLowercase().split("\n");
+        heroArray = data.toLowerCase().split("\n");
         $("#search-input").autocomplete({
             source: heroArray
         }).focus(function() {
             $(this).autocomplete("search", "");
         });
+
+        // populate gif and info divs with a random character on page load. Pulls from a small set of popular characters
+        var startingHeroes = ["batman", "deadpool", "thor", "spider-man", "wonder woman", "joker", "harley quinn", "rogue", "robin", "black widow"];
+        displayHeroInfo(startingHeroes[Math.floor(Math.random() * 11)]);
      });
 };
 
 function displayHeroInfo() {
-    var hero = $("#search-input").val().toLowercase();
-    var heroId = (heroArray.indexOf(hero) - 1);
+    var hero = $("#search-input").val().toLowerCase();
+    var heroId = (heroArray.indexOf(hero) + 1);
     var queryURLOne = "https://www.superheroapi.com/api.php/10158163759470734/" + heroId;
     var queryURLTwo = "https://api.giphy.com/v1/gifs/search?api_key=UZ1q06vU6ySOGMpaTwRtjIXmWHoGeJjg&q=" + hero + "&limit=5&offset=0&rating=G&lang=en";
 
